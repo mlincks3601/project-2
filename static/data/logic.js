f88ec3f21a81dd8a14cee20a9723195be0f01487
mlincks3601 <mlincks3601@gmail.com>
2021-07-15 18:58:11 -0400

    added map layer

--- static/data/logic.js
+++ static/data/logic.js
@@ -1,91 +1,106 @@
 // Add a tile layer (the background map image) to our map
 // We use the addTo method to add objects to our map
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.opens
-  tileSize: 512,
+// L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  // attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.opens
+  // tileSize: 512,
+  // maxZoom: 18,
+  // zoomOffset: -1,
+  // id: "mapbox/streets-v11",
+  // accessToken: API_KEY
+// }).addTo(myMap);
+// 
+
+// Creating map object
+var myMap = L.map("map-id", {
+  center: [31.51, -96.42],
+  zoom: 8
+});
+
+// Create the tile layer that will be the background of our map
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
+  id: "light-v10",
   accessToken: API_KEY
 }).addTo(myMap);
-
+// console.log(API_KEY);
 
 
 // Initialize all of the LayerGroups we'll be using
-var layers = {
-  2005: new L.LayerGroup(),
-  2010: new L.LayerGroup(),
-  2015: new L.LayerGroup(),
-  2019: new L.LayerGroup(),
-};
+// var layers = {
+  // 2005: new L.LayerGroup(),
+  // 2010: new L.LayerGroup(),
+  // 2015: new L.LayerGroup(),
+  // 2019: new L.LayerGroup(),
+// };
 
 // Create the map with our layers
-var map = L.map("map-id", {
-  center: [42.88, -97.38],
-  zoom: 5,
-  layers: [
-    layers.2005,
-    layers.2010,
-    layers.2015,
-    layers.2019,
-  ]
-});
-
+// var map = L.map("map-id", {
+  // center: [42.88, -97.38],
+  // zoom: 5,
+  // layers: [
+    // layers.2005,
+    // layers.2010,
+    // layers.2015,
+    // layers.2019,
+  // ]
+// });
+// 
 // Add our 'lightmap' tile layer to the map
-lightmap.addTo(map);
+// lightmap.addTo(map);
 
 // Create an overlays object to add to the layer control
-var overlays = {
-  "Tour 2005": layers.2005,
-  "Tour 2010": layers.2010,
-  "Tour 2015": layers.2015,
-  "Tour 2019": layers.2019,
-};
-
+// var overlays = {
+  // "Tour 2005": layers.2005,
+  // "Tour 2010": layers.2010,
+  // "Tour 2015": layers.2015,
+  // "Tour 2019": layers.2019,
+// };
+// 
 // Create a control for our layers, add our overlay layers to it
-L.control.layers(null, overlays).addTo(map);
+// L.control.layers(null, overlays).addTo(map);
 
 // Create a legend to display information about our map
-var info = L.control({
-  position: "bottomright"
-});
+// var info = L.control({
+  // position: "bottomright"
+// });
 
 // When the layer control is added, insert a div with the class of "legend"
-info.onAdd = function() {
-  var div = L.DomUtil.create("div", "legend");
-  return div;
-};
+// info.onAdd = function() {
+  // var div = L.DomUtil.create("div", "legend");
+  // return div;
+// };
 // Add the info legend to the map
-info.addTo(map);
+// info.addTo(map);
 
 // Initialize an object containing icons for each layer group
-var icons = {
-  2005: L.ExtraMarkers.icon({
-    icon: "ion-settings",
-    iconColor: "white",
-    markerColor: "yellow",
-    shape: "star"
-  }),
-  2010: L.ExtraMarkers.icon({
-    icon: "ion-android-bicycle",
-    iconColor: "white",
-    markerColor: "red",
-    shape: "circle"
-  }),
-  2015: L.ExtraMarkers.icon({
-    icon: "ion-minus-circled",
-    iconColor: "white",
-    markerColor: "blue-dark",
-    shape: "penta"
-  }),
-  2019: L.ExtraMarkers.icon({
-    icon: "ion-android-bicycle",
-    iconColor: "white",
-    markerColor: "orange",
-    shape: "circle"
-  }),
-};
-
+// var icons = {
+  // 2005: L.ExtraMarkers.icon({
+    // icon: "ion-settings",
+    // iconColor: "white",
+    // markerColor: "yellow",
+    // shape: "star"
+  // }),
+  // 2010: L.ExtraMarkers.icon({
+    // icon: "ion-android-bicycle",
+    // iconColor: "white",
+    // markerColor: "red",
+    // shape: "circle"
+  // }),
+  // 2015: L.ExtraMarkers.icon({
+    // icon: "ion-minus-circled",
+    // iconColor: "white",
+    // markerColor: "blue-dark",
+    // shape: "penta"
+  // }),
+  // 2019: L.ExtraMarkers.icon({
+    // icon: "ion-android-bicycle",
+    // iconColor: "white",
+    // markerColor: "orange",
+    // shape: "circle"
+  // }),
+// };
+// 
 // Perform an API call to the Citi Bike Station Information endpoint
 // d3.json("https://gbfs.citibikenyc.com/gbfs/en/station_information.json").then(function(infoRes) {
 // 
@@ -96,12 +111,12 @@ var icons = {
     // var stationInfo = infoRes.data.stations;
 // 
     //Create an object to keep of the number of markers in each layer
-    var city_counts = {
-      2005: 0,
-      2010: 0,
-      2015: 0,
-      2019: 0,
-    };
+    // var city_counts = {
+      // 2005: 0,
+      // 2010: 0,
+      // 2015: 0,
+      // 2019: 0,
+    // };
 // 
     //Initialize a stationStatusCode, which will be used as a key to access the appropriate layers, icons, and station c
     // var stationStatusCode;
@@ -152,13 +167,13 @@ var icons = {
 // });
 // 
 //Update the legend's innerHTML with the last updated time and station count
- function updateLegend(time, stationCount) {
-   document.querySelector(".legend").innerHTML = [
-    "<p>Updated: " + moment.unix(time).format("h:mm:ss A") + "</p>",
-    "<p class='2005'>Tour 2005: " + city_count.2005 + "</p>",
-    "<p class='coming-soon'>Stations Coming Soon: " + city_count.2010 + "</p>",
-    "<p class='empty'>Empty Stations: " + city_count.2015 + "</p>",
-   ].join("");
- }
-updateLegend();
+//  function updateLegend(time, stationCount) {
+  //  document.querySelector(".legend").innerHTML = [
+    // "<p>Updated: " + moment.unix(time).format("h:mm:ss A") + "</p>",
+    // "<p class='2005'>Tour 2005: " + city_count.2005 + "</p>",
+    // "<p class='coming-soon'>Stations Coming Soon: " + city_count.2010 + "</p>",
+    // "<p class='empty'>Empty Stations: " + city_count.2015 + "</p>",
+  //  ].join("");
+//  }
+// updateLegend();
 
