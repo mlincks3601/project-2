eae14ae2c35cdb73c6fd83082a1f8d36966b72ca
Tman <76560394+Tman2111@users.noreply.github.com>
2021-07-17 13:59:55 -0400

    classes in models

--- band_tours/models.py
+++ band_tours/models.py
@@ -1,13 +1,29 @@
 from .app import db
 
 
-class Pet(db.Model):
-    __tablename__ = 'pets'
+class cities(db.Model):
+    __tablename__ = 'cities'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
+    
+    city = db.Column(db.String(64))
+    state = db.Column(db.String(64))
     lat = db.Column(db.Float)
-    lon = db.Column(db.Float)
+    long = db.Column(db.Float)
+    id = db.Column(db.Integer, primary_key=True)
 
     def __repr__(self):
-        return '<Pet %r>' % (self.name)
+        return '<cities %r>' % (self.name)
+
+class cities(db.Model):
+    __tablename__ = 'tours'
+
+    city_id = db.Column(db.Integer, foreign_key=True)
+    city = db.Column(db.String(64))
+    state = db.Column(db.String(64))
+    country = db.Column(db.String(60))
+    year = db.Column(db.Integer)
+    band = db.Column(db.String(100))
+    
+
+    def __repr__(self):
+        return '<tours %r>' % (self.name)
