b085fd50a348b005a7fbf32e0886cf102f611a32
Tman <76560394+Tman2111@users.noreply.github.com>
2021-07-19 18:49:18 -0400

    first api halway point

--- band_tours/app.py
+++ band_tours/app.py
@@ -8,7 +8,7 @@ from flask import (
     request,
     redirect)
 
-from sqlalchemy import create_engine
+from sqlalchemy import func
 
 # from config import (username, pw)
 
@@ -20,17 +20,17 @@ app = Flask(__name__)
 #################################################
 # Database Setup
 #################################################
-connectionstring = "postgres:Tman2111@localhost:5432/project-2"
-engine = create_engine(f'postgresql://{connectionstring}')
+connectionstring = "postgresql+psycopg2://postgres:Tman2111@localhost:5432/project-2"
+#engine = create_engine(f'postgresql+psycopg2://{connectionstring}')
 from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or engine
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or connectionstring
 
 # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-from .models import result
+from .models import Tours
 #from  .models import Tours, Cities
 
 
@@ -43,46 +43,52 @@ def home():
 @app.route("/api/bandmiles/<band>")
 def bandmiles(band):
     # results = db.session.query(Pet.name, Pet.lat, Pet.lon).all()
-    result = db.session.query(result).all()
+    results = db.session.query(Tours).all()
+    
+    for r in results:
+        print(r)
 
     # hover_text = [result[0] for result in results]
-    # lat = [result[1] for result in results]
-    # lon = [result[2] for result in results]
+    lat = [result for result in results]
+    lon = [result for result in results]
 
-    # pet_data = [{
+    tour_data = [{
     #     "type": "scattergeo",
     #     "locationmode": "USA-states",
-    #     "lat": lat,
-    #     "lon": lon,
+        #  "lat": lat,
+        #  "lon": lon,
     #     "text": hover_text,
     #     "hoverinfo": "text",
-    #     "marker": {
-    #         "size": 15,
-    #         "line": {
-    #             "color": "rgb(8,8,8)",
-    #             "width": 1
-    #         },
-    #     }
+         "marker": {
+            "size": 15,
+            "line": {
+                "color": "rgb(8,8,8)",
+                "width": 1
+            },
+        }
     }]
 
     #
     #}]
 
-    return jsonify(Tours)
+    return jsonify(tour_data)
 
 @app.route("/api/bandyear/<band>/<year>")
 def bandyearloc(band, year):
-
+    band = band.upper()
+    print(band.upper())
+    results = db.session.query(Tours.city,Tours.lat, Tours.long).filter(func.upper(Tours.band)==band).all()
+    
+    output = [{'city': r.city, 'lat':r.lat, 'long':r.long} for r in results]
+    
     output_data = [
         {
             'band': band,
-            'year': year
+            'year': year,
+            'location': "city"
         }
     ]
-    return jsonify(output_data)
-
-
-
+    return jsonify(output)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- band_tours/models.py
+++ band_tours/models.py
@@ -15,15 +15,17 @@ class Cities(db.Model):
         return '<cities %r>' % (self.name)
 
 class Tours(db.Model):
-    __tablename__ = 'tours'
-
-    city_id = db.Column(db.Integer, foreign_key=True)
+    __tablename__ = 'tour'
+    id = db.Column(db.Integer, primary_key=True)
+    city_id = db.Column(db.Integer)
     city = db.Column(db.String(40))
     state = db.Column(db.String(40))
     country = db.Column(db.String(40))
     year = db.Column(db.Integer)
     band = db.Column(db.String(50))
+    lat = db.Column(db.String(10))
+    long = db.Column(db.String(10))
     
 
     def __repr__(self):
-        return '<tours %r>' % (self.name)
+        return '<tours %r %d>' % (self.city, self.year)
